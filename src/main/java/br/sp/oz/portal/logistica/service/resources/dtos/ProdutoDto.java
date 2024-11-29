10f3e82f64eb7dd2ea546f2cf7a40245f134e3a1
Douglas <douglas.szlps@gmail.com>
2024-11-29 12:47:03 -0300

    [feature Douglas - task: Implementacao do swagger doc v3 na controller Produto]

--- src/main/java/br/sp/oz/portal/logistica/service/resources/dtos/ProdutoDto.java
+++ src/main/java/br/sp/oz/portal/logistica/service/resources/dtos/ProdutoDto.java
@@ -3,6 +3,13 @@ package br.sp.oz.portal.logistica.service.resources.dtos;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -10,28 +17,41 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 public class ProdutoDto {
-
+	
+	@NotBlank
+	@Size(max = 300)
 	private String nome;
 	
+	@Size(max = 2500)
 	private String descricao;
 	
+	@NotBlank
 	private Float precoCompra;
 	
+	@NotBlank
 	private Float precoMedio;
 	
+	@NotBlank
 	private Float precoVenda;
 	
+	@NotBlank
 	private LocalDate validadeLoteData;
 	
 	private CategoriaProdutoDto categoriaProduto;
 	
+	@Size(min = 1)
 	private int qtdLote;
 	
-	private Endereco destinatario ;
+	private Endereco destinatario;
 	
 	private Endereco remetente;
 	
+	@DateTimeFormat(iso = ISO.DATE_TIME)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
 	private LocalDateTime dataHoraEntrada;
-
+	
+	
+	@DateTimeFormat(iso = ISO.DATE_TIME)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
 	private LocalDateTime dataHoraSaida; 
 }
